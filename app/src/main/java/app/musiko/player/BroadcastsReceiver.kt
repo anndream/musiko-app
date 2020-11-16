6a3cbb604be3dce1710b02f1177b67ea5878819d
hebertcisco <mrrabbitomg@gmail.com>
2020-11-15 21:59:05 -0300

    add NotificationReceiver

---
+++ app/src/main/java/app/musiko/player/BroadcastsReceiver.kt
@@ -0,0 +1,79 @@
+package app.musiko.player
+
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
+import app.musiko.MusikoConstants
+import app.musiko.goPreferences
+
+// The headset connection states (0,1)
+private const val HEADSET_DISCONNECTED = 0
+private const val HEADSET_CONNECTED = 1
+
+class NotificationReceiver(
+        private val playerService: PlayerService,
+        private val mediaPlayerHolder: MediaPlayerHolder
+) : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        val action = intent?.action
+
+        if (action != null) {
+            mediaPlayerHolder.apply {
+                when (action) {
+                    MusikoConstants.PREV_ACTION -> instantReset()
+                    MusikoConstants.PLAY_PAUSE_ACTION -> resumeOrPause()
+                    MusikoConstants.NEXT_ACTION -> skip(true)
+                    MusikoConstants.REPEAT_ACTION -> {
+                        mediaPlayerHolder.repeat(true)
+                        mediaPlayerInterface.onUpdateRepeatStatus()
+                    }
+                    MusikoConstants.CLOSE_ACTION -> if (playerService.isRunning && isMediaPlayer) {
+                        stopPlaybackService(
+                                stopPlayback = true
+                        )
+                    }
+
+                    BluetoothDevice.ACTION_ACL_DISCONNECTED -> if (isCurrentSong && goPreferences.isHeadsetPlugEnabled) {
+                        pauseMediaPlayer()
+                    }
+                    BluetoothDevice.ACTION_ACL_CONNECTED -> if (isCurrentSong && goPreferences.isHeadsetPlugEnabled) {
+                        resumeMediaPlayer()
+                    }
+
+                    AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED ->
+                        when (intent.getIntExtra(AudioManager.EXTRA_SCO_AUDIO_STATE, -1)) {
+                            AudioManager.SCO_AUDIO_STATE_CONNECTED -> if (isCurrentSong && goPreferences.isHeadsetPlugEnabled) {
+                                resumeMediaPlayer()
+                            }
+                            AudioManager.SCO_AUDIO_STATE_DISCONNECTED -> if (isCurrentSong && goPreferences.isHeadsetPlugEnabled) {
+                                pauseMediaPlayer()
+                            }
+                        }
+
+                    Intent.ACTION_HEADSET_PLUG -> if (isCurrentSong && goPreferences.isHeadsetPlugEnabled) {
+                        when (intent.getIntExtra("state", -1)) {
+                            // 0 means disconnected
+                            HEADSET_DISCONNECTED -> if (isCurrentSong && goPreferences.isHeadsetPlugEnabled) {
+                                pauseMediaPlayer()
+                            }
+                            // 1 means connected
+                            HEADSET_CONNECTED -> if (isCurrentSong && goPreferences.isHeadsetPlugEnabled) {
+                                resumeMediaPlayer()
+                            }
+                        }
+                    }
+                    AudioManager.ACTION_AUDIO_BECOMING_NOISY -> if (isPlaying && goPreferences.isHeadsetPlugEnabled) {
+                        pauseMediaPlayer()
+                    }
+                }
+            }
+        }
+        if (isOrderedBroadcast) {
+            abortBroadcast()
+        }
+    }
+}
